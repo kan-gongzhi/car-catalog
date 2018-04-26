31402563233ee6ed533f260c2ae6579954b9cb50
Kyle Kan <kangongzhi@gmail.com>
2018-04-26 22:04:19 +1000

    updated no image available placeholder image

--- src/ui/components/Image/Image.js
+++ src/ui/components/Image/Image.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './Image.css';
+import placeholder from './car-placeholder.png';
 
 export const handleError = e => {
-  e.target.src = 'http://via.placeholder.com/350x150';
+  e.target.src = placeholder;
 };
 const Image = ({ src, className = '' }) => (
   <img src={src} className={`Image ${className}`} onError={handleError} />
